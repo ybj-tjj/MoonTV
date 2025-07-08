44d03ce1bd3ba0157837b5bff6907ee4ba3d4ce1
ybj-tjj <154504827+ybj-tjj@users.noreply.github.com>
2025-07-08 13:36:39 +0800

    Update SiteProvider.tsx

--- src/components/SiteProvider.tsx
+++ src/components/SiteProvider.tsx
@@ -4,9 +4,9 @@ import { createContext, ReactNode, useContext } from 'react';
 
 const SiteContext = createContext<{ siteName: string; announcement?: string }>({
   // 默认值
-  siteName: 'MoonTV',
+  siteName: '杨忠强TV',
   announcement:
-    '本网站仅提供影视信息搜索服务，所有内容均来自第三方网站。本站不存储任何视频资源，不对任何内容的准确性、合法性、完整性负责。',
+    '欢迎来到，我搭建的影视网页！',
 });
 
 export const useSite = () => useContext(SiteContext);
